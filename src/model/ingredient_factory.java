57adfc5a305fb929b367893a8ce812e64e2db681
Pirvulescu Daria <dariapirvulescu@yahoo.ro>
2024-04-09 15:11:50 +0300

    Initial commit

---
+++ src/model/ingredient_factory.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ingredient {
+    private String name;
+    private int quantity;
+
+    public ingredient(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
